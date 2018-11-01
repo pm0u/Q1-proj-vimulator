9c29b43c29d1a298c32faa84eb0ab82340745f83
Paul Mourer <paul.mourer@gmail.com>
2018-11-01 14:36:57 -0600

    initiated repo with mocha and chai testing files

---
+++ test/tests.js
@@ -0,0 +1,11 @@
+const chai = require('chai')
+const assert = chai.assert
+const expect = chai.expect
+const should = chai.should()
+const main = require('../src/main.js')
+
+describe('chai/mocha install', function () {
+  it('worked?', function () {
+    expect(true).to.equal(true)
+  })
+})
