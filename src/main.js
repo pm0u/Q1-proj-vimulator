c28bafc580016d37c93464e15f573a1e797c086b
Paul Mourer <paul.mourer@gmail.com>
2018-11-01 19:38:23 -0600

    Cursor placed on page load (setCursor, placeCursor)
    added function getRow to separate placing cursor from row selection.
    added onload.js to be run at page load, separate functions. cursor
    initializes to first char of first row on page load!

--- src/main.js
+++ src/main.js
@@ -6,23 +6,18 @@ let cursorCol = 0;
 
 function placeCursor ( text='', cursorCol=0) {
   // takes desired cursor col/row
-  // takes innerHTML
+  // places within provided text
   // inserts new span at proper location
   let spanPre = '<span class="mode-normal-cursor">'
   let spanPost = '</span>'
   let newText = ''
   cursorCol = ( cursorCol >= text.length ? text.length - 1 : cursorCol)
-
-
   newText = `${text.substring(0,cursorCol)}${spanPre}${text.charAt(cursorCol)}${spanPost}${text.substring(cursorCol+1)}`
-
   return newText
-
 }
 
 function removeCursor () {
-  //takes innerHTML, removes cursor span text
-  //returns clean innerHTML with no cursor span
+  // takes text w/ span, returns text w/o span
 
 }
 
@@ -34,28 +29,12 @@ function moveCursor () {
 
 }
 
+function setCursor (element, cursorCol=0, cursorRow=0) {
+  //parent function for cursor movement actions
+  let row = getRow(element,cursorRow)
+  row.innerHTML = placeCursor(row.innerHTML, cursorCol)
+}
 
-
-
-
-//document.addEventListener('DOMContentLoaded', (event) => {
-  //setCursor() // set initial cursor position
-
-
-
-
-//});
-//// function that makes cursor background dark over character
-
-
-
-
-///need event listeners to move this background.... maybe move span in innerHTML?
-
-
-///keep track of row and col somehow
-
-
-module.exports = {
-  placeCursor: placeCursor
+function getRow (element, row=0) {
+    return element.children[row]
 }
---
+++ src/onload.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+let vimText = document.getElementById('vim-text')
+let vimTextIH = vimText.innerHTML
+let cursorRow = 0;
+let cursorCol = 0;
+
+setCursor(vimText)
+
+});
